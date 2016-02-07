ac398166c8c8fc1bdc4cca53c6c1f5b1bed81b93
kkaufman6 <raizenxx@gmail.com>
2016-02-07 22:57:34 +0000

    created cardFlip.js

---
+++ FlashCardApp/app/assets/javascripts/cardFlip.js
@@ -0,0 +1,20 @@
+;$(function() {
+  //Click "back" to see back of card
+  $('#flipCardToBack').click(function(){
+    $('#flashCardFront').css({
+      display: 'none'
+    })
+    $('#flashCardBack').css({
+      display: 'block'
+    })
+  })
+  //Click "front" to see front of card
+  $('#flipCardToFront').click(function(){
+    $('#flashCardFront').css({
+      display: 'block'
+    })
+    $('#flashCardBack').css({
+      display: 'none'
+    })
+  })
+});
