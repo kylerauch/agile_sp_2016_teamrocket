60c402acc9cc2e74f1c7d974041b34d4c111d9e3
kkaufman6 <raizenxx@gmail.com>
2016-02-10 18:52:07 +0000

    changed id's to classes for flashCard, flashCardFront, flashCardBack, flipCardToBack, and flipCardToFront in cardFlip.js and cards/show.html.erb

--- app/assets/javascripts/cardFlip.js
+++ app/assets/javascripts/cardFlip.js
@@ -1,20 +1,20 @@
 $(document).on('ready page:load', function () {
   ;$(function() {
     //Click "back" to see back of card
-    $('#flashCardFront').click(function(){
-      $('#flashCardFront').css({
+    $('.flashCardFront').click(function(){
+      $('.flashCardFront').css({
         display: 'none'
       }),
-      $('#flashCardBack').css({
+      $('.flashCardBack').css({
         display: 'table-cell'
       })
     })
     //Click "front" to see front of card
-    $('#flashCardBack').click(function(){
-      $('#flashCardFront').css({
+    $('.flashCardBack').click(function(){
+      $('.flashCardFront').css({
         display: 'table-cell'
       }),
-      $('#flashCardBack').css({
+      $('.flashCardBack').css({
         display: 'none'
       })
     })
