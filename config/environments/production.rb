9c29ec2dddbcc0269218e3559267e3417ec9a068
krauch1 <krauch1@student.cvtc.edu>
2016-02-26 00:51:05 +0000

    Configured devise to work on application and added logic for if the user is signed in

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  config.action_mailer_default_url_options = { host => 'localhost: 2525'}
+  
   Paperclip.options[:command_path] = "/usr/local/bin/"
 end
