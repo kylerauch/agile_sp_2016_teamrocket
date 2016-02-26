2cd8130ad2695bd3c2bda49e85e4d6fb8e3a772c
krauch1 <krauch1@student.cvtc.edu>
2016-02-26 21:01:34 +0000

    Got twitter validation and authentication to work

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'paperclip', '~> 4.3', '>= 4.3.5'
 gem 'devise'
 gem 'omniauth-twitter'
 #gem 'omniauth-facebook'
+gem 'protected_attributes'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225235225) do
+ActiveRecord::Schema.define(version: 20160226205026) do
 
   create_table "cards", force: :cascade do |t|
     t.string   "frontContent"
@@ -75,6 +75,9 @@ ActiveRecord::Schema.define(version: 20160225235225) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
