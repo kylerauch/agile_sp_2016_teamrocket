0a18695cb0bdbf39bca1a3349e13194ac7b1b8d7
Justin Bowe <justinbustin_8@hotmail.com>
2016-02-27 04:31:10 +0000

    Fixed the autheniticating using email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  #attr_accessible :email, :password, :password_confirmation, :remember_me, :username
          
   
   validates_presence_of :username
