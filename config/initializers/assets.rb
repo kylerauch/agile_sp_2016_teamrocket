e2db1c7d5a276812add9b49f3179585d9e687164
kkaufman6 <raizenxx@gmail.com>
2016-02-27 21:52:23 +0000

    removed precompiles for randomcard.js and cycleCards.js from initializers/assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,8 +3,6 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( cardFlip.js )
-Rails.application.config.assets.precompile += %w( randomcard.js )
-Rails.application.config.assets.precompile += %w( cycleThroughCards.js )
 Rails.application.config.assets.precompile += %w( decks.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
