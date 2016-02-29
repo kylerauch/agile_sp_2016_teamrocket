f7ef00bdbdecede2f1b0e911f436a656acbd838d
krauch1 <krauch1@student.cvtc.edu>
2016-02-29 14:17:28 +0000

    Added test case for decks in deck_test.rb

--- test/models/deck_test.rb
+++ test/models/deck_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class DeckTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+      #test "create a deck" do
+        #deck = Deck.new
+        #deck.title = "Cosby Show Characters"
+       # deck.description = "Jello Pudding"
+        #deck.created_by = "Fat Albert"
+    #assert deck.save 
+  #end
   
 end
